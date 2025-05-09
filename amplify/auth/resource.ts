122b562a1b028733eef3c9c74a0d697b56177ca9
Kunal Parkhade <kunalparkhade@gmail.com>
2025-05-09 10:39:47 +0530

    Versions updated

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth } from '@aws-amplify/backend-auth';
 
 /**
  * Define and configure your auth resource
@@ -9,3 +9,13 @@ export const auth = defineAuth({
     email: true,
   },
 });
+
+import { referenceAuth } from '@aws-amplify/backend-auth';
+
+// export const auth = referenceAuth({
+//   userPoolId: 'us-east-1_xxxx',
+//   identityPoolId: 'us-east-1:b57b7c3b-9c95-43e4-9266-xxxx',
+//   authRoleArn: 'arn:aws:iam::xxxx:role/amplify-xxxx-mai-amplifyAuthauthenticatedU-xxxx',
+//   unauthRoleArn: 'arn:aws:iam::xxxx:role/amplify-xxxx-mai-amplifyAuthunauthenticate-xxxx',
+//   userPoolClientId: 'xxxx',
+// });
